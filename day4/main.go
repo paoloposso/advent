e5d2cd30f91484a8b6f1f035b054c650c3cabd8b
Paolo Posso <pvictorsys@gmail.com>
2023-01-04 10:21:18 -0300

    Finish day 4

--- day4/main.go
+++ day4/main.go
@@ -8,6 +8,11 @@ import (
 )
 
 func main() {
+	// fmt.Println("part1", solvePart1())
+	fmt.Println("part2", solvePart2())
+}
+
+func solvePart1() int {
 	fileBytes, _ := ioutil.ReadFile("./input/input.txt")
 	allSectionPairs := strings.Split(string(fileBytes), "\n")
 
@@ -23,7 +28,7 @@ func main() {
 		}
 	}
 
-	fmt.Println(totalResult)
+	return totalResult
 }
 
 func fullyContains(sections []string) bool {
@@ -47,4 +52,40 @@ func fullyContains(sections []string) bool {
 	} else {
 		return false
 	}
+}
+
+func solvePart2() int {
+	fileBytes, _ := ioutil.ReadFile("./input/input.txt")
+	allSectionPairs := strings.Split(string(fileBytes), "\n")
+
+	totalResult := 0
+
+	for _, pair := range allSectionPairs {
+		sections := strings.Split(pair, ",")
+
+		fmt.Println(sections)
+
+		if overlap(sections) {
+			totalResult++
+		}
+	}
+
+	return totalResult
+}
+
+func overlap(sections []string) bool {
+	leftSection := sections[0]
+	rightSection := sections[1]
+
+	leftSectionStart, err := strconv.Atoi(strings.Split(leftSection, "-")[0])
+	leftSectionEnd, err := strconv.Atoi(strings.Split(leftSection, "-")[1])
+
+	rightSectionStart, err := strconv.Atoi(strings.Split(rightSection, "-")[0])
+	rightSectionEnd, err := strconv.Atoi(strings.Split(rightSection, "-")[1])
+
+	if err != nil {
+		panic("invalid range")
+	}
+
+	return !(leftSectionEnd < rightSectionStart || rightSectionEnd < leftSectionStart)
 }
